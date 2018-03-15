bba607a94202643ef85049dbd528a3c57a197e1c
moakem <moa13007@byui.edu>
2018-03-15 17:06:41 -0600

    Tutorial changed into LDS Movies Assignment

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                     new Movie
+                     {
+                         Title = "The Best Two Years",
+                         ReleaseDate = DateTime.Parse("2004-2-20"),
+                         Genre = "Comedy",
+                         Rating = "PG",
+                         Price = 15.00M
+                     },
+
+                     new Movie
+                     {
+                         Title = "The RM",
+                         ReleaseDate = DateTime.Parse("2003-1-31"),
+                         Genre = "Comedy",
+                         Rating = "PG",
+                         Price = 15.99M
+                     },
+
+                     new Movie
+                     {
+                         Title = "The Other Side of Heaven",
+                         ReleaseDate = DateTime.Parse("2002-4-12"),
+                         Genre = "Inspirational",
+                         Rating = "PG",
+                         Price = 6.45M
+                     },
+
+                   new Movie
+                   {
+                       Title = "Meet the Mormons",
+                       ReleaseDate = DateTime.Parse("2015-2-26"),
+                       Genre = "Documentary",
+                       Rating = "PG",
+                       Price = 6.99M
+                   },
+
+                    new Movie
+                    {
+                        Title = "Church Ball",
+                        ReleaseDate = DateTime.Parse("2006-3-17"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 6.53M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +16,25 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
